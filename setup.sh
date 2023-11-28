1208f4e84a2f54b70058879f247dfbb3b67c4e75
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2023-11-28 23:31:29 +0000

    update

---
+++ setup.sh
@@ -0,0 +1,5 @@
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia --solver=libmamba\
+# pip install torch \
+# torchvision \
+# torchaudio \
+# --index-url https://download.pytorch.org/whl/cu121
