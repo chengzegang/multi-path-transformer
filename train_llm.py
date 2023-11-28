6f9e2015b16ba3f837f5d8d913a845c4ef853dcf
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2023-11-27 19:39:01 -0500

    update

--- train_llm.py
+++ train_llm.py
@@ -350,7 +350,6 @@ def train(
                     )
                 model.eval()
                 torch.save(model.state_dict(), f"models/llm-{step}.pt")
-                torch.cuda.empty_cache()
 
     pbar.close()
 
