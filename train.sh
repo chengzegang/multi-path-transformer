71a0bcb0ae505d51d11244d433547fa30e5d74cc
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2023-11-29 00:43:33 +0000

    update

--- train.sh
+++ train.sh
@@ -3,7 +3,7 @@ NNODES=1
 NPROC_PER_NODE=$(nvidia-smi -L | wc -l)
 export NNODES=$NNODES
 export NPROC_PER_NODE=$NPROC_PER_NODE
-export WANDB_MODE=offline
+
 torchrun \
     --nnodes=$NNODES \
     --nproc-per-node=$NPROC_PER_NODE \
