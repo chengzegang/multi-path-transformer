65edd3f2752502b6b168495419b7a2a918d0d358
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2023-11-28 21:36:21 -0500

    update

--- train.sh
+++ train.sh
@@ -4,10 +4,10 @@ NNODES=1
 NPROC_PER_NODE=$(nvidia-smi -L | wc -l)
 export NNODES=$NNODES
 export NPROC_PER_NODE=$NPROC_PER_NODE
-export WANDB_MODE=offline
+
 torchrun \
     --nnodes=$NNODES \
     --nproc-per-node=$NPROC_PER_NODE \
     --rdzv-backend=c10d \
     --rdzv-endpoint=localhost:0 \
-    cli.py configs/100m-web-4a100-80gb.yml
+    cli.py configs/500m-pile-2a100-80gb.yml
