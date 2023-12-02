5de8824b2b7dd59ed0e97581b2724f1a9d663eba
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2023-12-02 05:03:22 +0000

    update

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia --solver=libmamba -y
 conda install transformers datasets pyarrow pandas evaluate accelerate sentencepiece wandb pynvml wandb typer jsonlines matplotlib -c conda-forge --solver=libmamba -y
+git config --add user.email "20091803+chengzegang@users.noreply.github.com"
+git config --add user.name "Zegang Cheng"
