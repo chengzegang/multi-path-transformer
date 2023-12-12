90a20a720eb55fe7f4bce8c3a0695ecadde97c0a
Zegang Cheng <20091803+chengzegang@users.noreply.github.com>
2023-12-12 02:02:35 -0500

    update

--- multipath/nn/modules.py
+++ multipath/nn/modules.py
@@ -456,20 +456,12 @@ class HKVAttention(nn.Module):
         q = self.q_proj(input_embeds)
         # q in shape (B, S, P, H)
         choices = None
-        if self.training:
-            choices = torch.randint(
-                0,
-                self.num_kv,
-                size=q.shape[:-1],
-                device=q.device,
-            )
-        else:
-            with torch.no_grad():
-                kv = self.kv_norm(self.kv)
-                kv = kv[None, None, None, ...]
-                kv = q.unsqueeze(-2) + kv  # (B, S, P, C, H)
-                k = self.k_proj(kv).transpose(-1, -2)
-                choices = torch.matmul(q.unsqueeze(-2), k).argmax(-1).squeeze(-1)
+        with torch.no_grad():
+            kv = self.kv_norm(self.kv)
+            kv = kv[None, None, None, ...]
+            kv = q.unsqueeze(-2) + kv  # (B, S, P, C, H)
+            k = self.k_proj(kv).transpose(-1, -2)
+            choices = torch.matmul(q.unsqueeze(-2), k).argmax(-1).squeeze(-1)
         chosen_kv = self.kv[choices]
         chosen_kv = self.kv_norm(chosen_kv)
         kv = q + chosen_kv
